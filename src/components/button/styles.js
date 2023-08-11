97565350a7e2d30c026eeac1a0360c446d75b486
matheuscarvalhoo <matheuscarvalho98@outlook.com.br>
2023-08-11 14:41:07 -0300

    add button component

--- src/components/button/styles.js
+++ src/components/button/styles.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components'
 
 export const Container = styled.button`
+    width: 100%;
+    background-color: ${({theme}) => theme.COLORS.TOMATO_100};
+    color: ${({theme}) => theme.COLORS.LIGHT_100};
+    height: 35px;
+
     
 `
